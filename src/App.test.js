4757dad87ce1efd049243a5f34f70c61eac6669b
Juha Tauriainen <juha@bin.fi>
2023-11-01 00:07:44 +0200

    Update test case to find hei maailma text

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders hei maailma link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/hei maailma/i);
   expect(linkElement).toBeInTheDocument();
 });
